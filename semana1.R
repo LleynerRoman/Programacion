d877b5d37af4eff9f9fed21a5543eafeefa95afa
unknown <leyenerromanpe@gmail.com>
2024-09-02 09:50:15 -0500

    mi primer repositorio

---
+++ semana1.R
@@ -0,0 +1,11 @@
+notas<-c(15,16,18,19,15) |>
+  mean()
+library(tidyverse)
+notas<-c(15,17,18,16,14) %>% +1 %>% 
+  mean()
+
+notas<-c(15,17,18,16,14) %>% +1 %>% 
+  mean()
+notas<-c(15,16,18,19,15|>
+           mean()
+           
